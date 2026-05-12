0ea476457fcc03cf16bd92e4e8c85600bcae8052
raftaar1191 <raftaar1191@gmail.com>
2026-05-12 14:48:43 +0530

    [Spec Kit] Add analysis report

--- includes/Modules/Sitewide/AcrossAI_Sitewide_Module.php
+++ includes/Modules/Sitewide/AcrossAI_Sitewide_Module.php
@@ -12,6 +12,7 @@ namespace AcrossAI_Abilities_Manager\Includes\Modules\Sitewide;
 use AcrossAI_Abilities_Manager\Includes\Base\AcrossAI_Module_Base;
 use AcrossAI_Abilities_Manager\Includes\Loader;
 use AcrossAI_Abilities_Manager\Includes\Modules\Sitewide\Database\AcrossAI_Sitewide_Query;
+use AcrossAI_Abilities_Manager\Includes\Modules\Sitewide\Database\AcrossAI_Sitewide_Table;
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
@@ -41,6 +42,10 @@ class AcrossAI_Sitewide_Module extends AcrossAI_Module_Base {
 	 * @return void
 	 */
 	public function register_hooks( Loader $loader ): void {
+		// Instantiate on every request so BerlinDB registers $wpdb->acrossai_abilities_overwrite
+		// and hooks maybe_upgrade() to admin_init (creates/upgrades the table).
+		new AcrossAI_Sitewide_Table();
+
 		$controller = new AcrossAI_Sitewide_Rest_Controller( new AcrossAI_Sitewide_Query() );
 		$loader->add_action( 'rest_api_init', $controller, 'register_routes' );
 	}
